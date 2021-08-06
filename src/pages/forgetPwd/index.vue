ef45c2589232725a730b71dfbf912fd86de82e25
luzhen001 <luzhenlimengcai@qq.com>
2021-08-06 09:20:13 +0800

    修改密码

--- src/pages/forgetPwd/index.vue
+++ src/pages/forgetPwd/index.vue
@@ -11,14 +11,14 @@
                         <a-tab-pane tab="忘记密码" key="1">
                             <a-form-item>
                                 <a-input size="large" placeholder="请输入手机号码">
-                                    <a-icon slot="prefix" type="mobile" />
+                                    <a-icon slot="prefix" type="mobile" class="icon_16" />
                                 </a-input>
                             </a-form-item>
                             <a-form-item>
                                 <a-row :gutter="8" style="margin: 0 -4px">
                                     <a-col :span="16">
                                         <a-input size="large" placeholder="请输入验证码">
-                                            <a-icon slot="prefix" type="mail" />
+                                            <a-icon slot="prefix" type="mail" class="icon_16" />
                                         </a-input>
                                     </a-col>
                                     <a-col :span="8" style="padding-left: 4px">
@@ -28,12 +28,12 @@
                             </a-form-item>
                             <a-form-item>
                                 <a-input size="large" placeholder="请输入密码" autocomplete="autocomplete" type="password" v-decorator="['password', {rules: [{ required: false, message: '请输入密码', whitespace: true}]}]">
-                                    <a-icon slot="prefix" type="lock" />
+                                    <a-icon slot="prefix" type="lock" class="icon_16" />
                                 </a-input>
                             </a-form-item>
                             <a-form-item>
                                 <a-input size="large" placeholder="请输入确认密码" autocomplete="autocomplete" type="password" v-decorator="['password', {rules: [{ required: false, message: '请输入确认密码', whitespace: true}]}]">
-                                    <a-icon slot="prefix" type="lock" />
+                                    <a-icon slot="prefix" type="lock" class="icon_16" />
                                 </a-input>
                             </a-form-item>
                             <a-form-item style="margin-top: 34px">
@@ -112,6 +112,9 @@ export default {
                     .captcha-button {
                         font-size: 14px;
                     }
+                }
+                /deep/ .ant-tabs-bar{
+                    margin: 0 0 30px 0;
                 }
             }
         }
